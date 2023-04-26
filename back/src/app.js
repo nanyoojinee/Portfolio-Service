1750525b7be0c53ad2d5d169b9d5ad6f24ee97d7
[AI7/2반] 변상기 <tkdrl112@gmail.com>
2023-04-26 14:36:12 +0900

    프로젝트, 자격증 MVP

--- back/src/app.js
+++ back/src/app.js
@@ -2,7 +2,7 @@ import cors from "cors";
 import express from "express";
 import { awardRouter } from "./routers/awardRouter";
 import { errorMiddleware } from "./middlewares/errorMiddleware";
-import { userAuthRouter } from "./routers/userRouter";
+import { certificateRouter } from "./routers/certificateRouter";
 
 const app = express();
 
@@ -22,7 +22,8 @@ app.get("/", (req, res) => {
 
 // router, service 구현 (userAuthRouter는 맨 위에 있어야 함.)
 app.use(userAuthRouter);
-app.use(awardRouter);
+app.use(projectRouter);
+app.use(certificateRouter);
 
 // 순서 중요 (router 에서 next() 시 아래의 에러 핸들링  middleware로 전달됨)
 app.use(errorMiddleware);
