d457a3aeb3d187497ea05a5d1d7efd1f67863bb6
Yeon <dlgpdusls2@gmail.com>
2023-04-27 22:39:29 +0900

    modify: certificate 수정

--- back/src/db/models/Certificate.js
+++ back/src/db/models/Certificate.js
@@ -1,37 +1,39 @@
 import { CertificateModel } from "../schemas/certificate";
 
 class Certificate {
-    static async create({ newCertificate }) {
-        const createdNewCertificate = await CertificateModel.create(newCertificate);
-        return createdNewCertificate;
-    }
-    static async findById({ CertificateId }) {
-        const certificate = await CertificateModel.findOne({id: CertificateId});
-        return certificate;
-    }
-    static async findByUserId({ user_id}) {
-        const certificates = await CertificateModel.find({user_id});
-        return certificates;
-    }
+  static async create({ newCertificate }) {
+    const createdNewCertificate = await CertificateModel.create(newCertificate);
+    return createdNewCertificate;
+  }
 
-    static async update({ certificateId, fieldToUpdate, newValue}) {
-        const filter = { id: certificateId};
-        const update = { [fieldToUpdate]: newValue};
-        const option = { returnOriginal: false};
+  static async findById({ certificateId }) {
+    const certificate = await CertificateModel.findOne({ id: certificateId });
+    return certificate;
+  }
 
-        const updatedCertificate = await CertificateModel.findOneAndUpdate(
-            filter,
-            update,
-            option
-        );
-        return updatedCertificate
-    }
+  static async findByUserId({ user_id }) {
+    const certificates = await CertificateModel.find({ user_id });
+    return certificates;
+  }
 
-    static async deleteById({certificateId}) {
-        const deleteCertificate = await AwardModel.deleteOne({id: certificateId});
-        const checkCertificateDeleted = deleteCertificate.deletedCount === 1;
-        return checkCertificateDeleted
-    }
+  static async update({ certificateId, fieldToUpdate, newValue }) {
+    const filter = { id: certificateId };
+    const update = { [fieldToUpdate]: newValue };
+    const option = { returnOriginal: false };
+
+    const updatedCertificate = await CertificateModel.findOneAndUpdate(
+      filter,
+      update,
+      option
+    );
+    return updatedCertificate;
+  }
+
+  static async deleteById({ certificateId }) {
+    const deleteResult = await CertificateModel.deleteOne({ id: certificateId });
+    const isDataDeleted = deleteResult.deletedCount === 1;
+    return isDataDeleted;
+  }
 }
 
-export {Certificate}
+export { Certificate };
