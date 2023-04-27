08bbec3c7360000e5a04ea05a7c28195c75b2a54
Yeon <dlgpdusls2@gmail.com>
2023-04-27 22:39:29 +0900

    modify: certificate 수정

--- back/src/routers/certificateRouter.js
+++ back/src/routers/certificateRouter.js
@@ -1,95 +1,96 @@
-import is from "@sindresorhus/is"
-import {Router} from 'express';
-import {login_required} from '../middlewares/login_required';
-import { certificateService } from '../services/certificateService';
+import is from "@sindresorhus/is";
+import { Router } from "express";
+import { login_required } from "../middlewares/login_required";
+import { CertificateService } from "../services/certificateService";
 
 const certificateRouter = Router();
 certificateRouter.use(login_required);
 
-certificateRouter.post('/certificate/create', async function (req,res,next) {
-    try{
-        if (is.emptyObject(req.body)) {
-            throw new Error(
-                "headers의 Content-Type을 application/json으로 설정해주세요"
-            );
-        }
-        // req (request) 에서 데이터 가져오기
-        const user_id = req.body.user_id;
-        const certificateName = req.body.certificateName;
-        const certificateDetail = req.body.certificateDetail;
-
-        // 위 데이터를 프로젝트 db에 추가하기
-        const newCertificate = await certificateService.addCertificate({
-            user_id,
-            certificateName,
-            certificateDetail
-        })
-
-        res.status(201).json(newCertificate);
-    } catch (error) {
-        next(error);
+certificateRouter.post("/certificate/create", async function (req, res, next) {
+  try {
+    if (is.emptyObject(req.body)) {
+      throw new Error(
+        "headers의 Content-Type을 application/json으로 설정해주세요"
+      );
     }
+
+    const user_id = req.body.user_id;
+    const certificateName = req.body.certificateName;
+    const certificateDetail = req.body.certificateDetail;
+
+    const newCertificate = await CertificateService.addCertificate({
+      user_id,
+      certificateName,
+      certificateDetail,
+    });
+
+    res.status(201).json(newCertificate);
+  } catch (error) {
+    next(error);
+  }
 });
 
-certificateRouter.get("/certificates/:id", async function (req,res,next) {
-    try {
-        const certificateId = req.params.id;
+certificateRouter.get("/certificates/:id", async function (req, res, next) {
+  try {
+    const certificateId = req.params.id;
 
-        const certificate = await certificateService.getAward({certificateId});
+    const certificate = await CertificateService.getCertificate({ certificateId });
 
-        if([certificate].errorMessage) {
-            throw new Error(certificate.errorMessage)
-        }
-        res.status(200).send(certificate);
-    } catch (error) {
-        next(error);
+    if (certificate.errorMessage) {
+      throw new Error(certificate.errorMessage);
     }
-})
 
-certificateRouter.put('/certificates/:id', async function(req,res,next) {
-    try {
-        //URI로 사용자가 작성한 프로젝트를 추출함.
-        const certificateId = req.params.id;
-        const certificateName = req.body.certificateName ?? null;
-        const certificateDetail = req.body.certificateDetail ?? null;
-
-        const toUpdate = {certificateName, certificateDetail};
-
-        // 해당 Object id로 Certificate 정보를 찾아 업데이트함. 업데이트 요소가 없을시 생략
-        const updatedCertificate = await certificateService.setCertificate({certificateId, toUpdate});
-
-        if (updatedCertificate.errorMessage) {
-            throw new Error(updatedCertificate.errorMessage);
-        }
-        res.status(200).json(updatedCertificate)
-    } catch (error) {
-        next(error);
-    }
+    res.status(200).send(certificate);
+  } catch (error) {
+    next(error);
+  }
 });
 
-certificateRouter.delete("certificates/:id", async function (req,res,next) {
-    try {
-        const certificateId = req.params.id;
+certificateRouter.put("/certificates/:id", async function (req, res, next) {
+  try {
+    const certificateId = req.params.id;
 
-        const deleteCertificate = await certificateService.deleteCertificate({ certificateId });
+    const certificateName = req.body.certificateName ?? null;
+    const certificateDetail = req.body.certificateDetail ?? null;
 
-        if(deleteCertificate.errorMessage) {
-            throw new Error(deleteCertificate.errorMessage);
-        }
-        res.status(200).send(deleteCertificate);
-    } catch (error) {
-        next(error);
+    const toUpdate = { certificateName, certificateDetail };
+
+    const certificate = await CertificateService.setCertificate({ certificateId, toUpdate });
+
+    if (certificate.errorMessage) {
+      throw new Error(certificate.errorMessage);
     }
-})
 
-certificateRouter.get("certificatelist/:user_id", async function (req,res,next) {
-    try {
-        const user_id = req.params.user_id;
-        const certificateList = await certificateService.getCertificateList({ user_id });
-        res.status(200).send(certificateList);
-    } catch (error) {
-        next(error);
+    res.status(200).send(certificate);
+  } catch (error) {
+    next(error);
+  }
+});
+
+certificateRouter.delete("/certificates/:id", async function (req, res, next) {
+  try {
+    const certificateId = req.params.id;
+
+    const result = await CertificateService.deleteCertificate({ certificateId });
+
+    if (result.errorMessage) {
+      throw new Error(result.errorMessage);
     }
-})
 
-export {certificateRouter}
+    res.status(200).send(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+certificateRouter.get("/certificatelist/:user_id", async function (req, res, next) {
+  try {
+    const user_id = req.params.user_id;
+    const certificateList = await CertificateService.getCertificateList({ user_id });
+    res.status(200).send(certificateList);
+  } catch (error) {
+    next(error);
+  }
+});
+
+export { certificateRouter };
--- back/src/services/certificateService.js
+++ back/src/services/certificateService.js
@@ -1,63 +1,67 @@
-import {Certificate} from "../db";
-import { v4 as uuidv4} from "uuid";
+import { Certificate } from "../db";
+import { v4 as uuidv4 } from "uuid";
 
-class certificateService{
-    static async addCertificate({ user_id, certificateName, certificateDetail }) {
-        
-        const id = uuidv4();
+class CertificateService {
+  static async addCertificate({ user_id, certificateName, certificateDetail }) {
+    const id = uuidv4();
 
-        const newCertificate = {id, user_id, certificateName, certificateDetail};
-        const createdNewCertificate = await Certificate.create({ newCertificate });
-        createdNewCertificate.errorMessage = null;
+    const newCertificate = { id, user_id, certificateName, certificateDetail };
+    const createdNewCertificate = await Certificate.create({ newCertificate });
 
-        return createdNewCertificate;
-    }
-    
-    static async getCertificate({CertificateId}){
-        const certificate = await Certificate.findById({CertificateId});
-        if (!certificate) {
-            const errorMessage = "해당 id를 가진 자격증이 존재하지 않습니다.";
-            return {errorMessage};
-        }
-        return certificate;
+    return createdNewCertificate;
+  }
+
+  static async getCertificate({ certificateId }) {
+    const certificate = await Certificate.findById({ certificateId });
+    if (!certificate) {
+      const errorMessage =
+        "해당 id를 가진 자격증 데이터는 없습니다. 다시 한 번 확인해 주세요.";
+      return { errorMessage };
     }
 
-    static async getCertificateList({ user_id }) {
-        const certificate = await Certificate.findByUserId({user_id});
-        return certificate;
+    return certificate;
+  }
+
+  static async getCertificateList({ user_id }) {
+    const certificates = await Certificate.findByUserId({ user_id });
+    return certificates;
+  }
+
+  static async setCertificate({ certificateId, toUpdate }) {
+    let certificate = await Certificate.findById({ certificateId });
+
+    if (!certificate) {
+      const errorMessage =
+        "해당 id를 가진 자격증 데이터는 없습니다. 다시 한 번 확인해 주세요.";
+      return { errorMessage };
     }
 
-    static async setCertificate({ certificateId, toUpdate}) {
-        let certificate = await Certificate.findById({ certificateId })
-
-        if (!certificate) {
-            const errorMessage = "해당 id를 가진 자격증이 존재하지 않습니다."
-            return {errorMessage};
-        }
-
-        if (toUpdate.certificateName) {
-            const fieldToUpdate = 'certificateName';
-            const newValue = toUpdate.certificateName;
-            certificate = await Certificate.update({certificateId, fieldToUpdate, newValue})
-        }
-
-        if (toUpdate.certificateDetail) {
-            const fieldToUpdate = 'certificateDetail';
-            const newValue = toUpdate.certificateDetail;
-            certificate = await Certificate.update({certificateId, fieldToUpdate, newValue})
-        }
-        return certificate;
+    if (toUpdate.certificateName) {
+      const fieldToUpdate = "certificateName";
+      const newValue = toUpdate.certificateName;
+      certificate = await Certificate.update({ certificateId, fieldToUpdate, newValue });
     }
 
-    static async deleteCertificate({certificateId}) {
-        const checkCertificateDeleted = await Certificate.deleteById({certificateId});
-
-        if(!checkCertificateDeleted) {
-            const errorMessage = "해당 id를 가진 자격증이 존재하지 않습니다."
-            return {errorMessage}
-        }
-        return {status: "ok"}
+    if (toUpdate.certificateDetail) {
+      const fieldToUpdate = "certificateDetail";
+      const newValue = toUpdate.certificateDetail;
+      certificate = await Certificate.update({ certificateId, fieldToUpdate, newValue });
     }
+
+    return certificate;
+  }
+
+  static async deleteCertificate({ certificateId }) {
+    const isDataDeleted = await Certificate.deleteById({ certificateId });
+
+    if (!isDataDeleted) {
+      const errorMessage =
+        "해당 id를 가진 자격증 데이터는 없습니다. 다시 한 번 확인해 주세요.";
+      return { errorMessage };
+    }
+
+    return { status: "ok" };
+  }
 }
 
-export {certificateService};
+export { CertificateService };
